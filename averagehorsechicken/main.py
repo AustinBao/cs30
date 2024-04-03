c579cdcc74da3a0a791e4e0d590d36a6f345a8f9
Austin Bao <a.bao1@EDU.epsb.ca>
2024-04-03 10:00:21 -0600

    awehalkdjw

--- averagehorsechicken/main.py
+++ averagehorsechicken/main.py
@@ -17,31 +17,48 @@ WHITE = (255, 255, 255)
 RED = (200, 25, 25)
 BLACK = (0, 0, 0)
 BLUE = (111, 143, 175)
-
+LIGHTBLUE = (173, 216, 230)
 #define game variables
 TILE_SIZE = 100
-ROWS = SCREEN_HEIGHT // TILE_SIZE
-MAX_COLS = SCREEN_WIDTH // TILE_SIZE
+MAIN_ROWS = SCREEN_HEIGHT // TILE_SIZE
+MAIN_COLS = SCREEN_WIDTH // TILE_SIZE
 
+SMALL_ROWS = SCREEN_HEIGHT // 4
+SMALL_COLS = SCREEN_WIDTH // 4
+
+# Loading images
+background = pygame.image.load("imgs/background.png")
+background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def draw_bkg():
 	screen.fill(WHITE)
+	screen.blit(background, (0, 0))
 
-def draw_grid():
+
+def draw_main_grid():
 	#vertical lines
-	for c in range(MAX_COLS + 1):
+	for c in range(MAIN_COLS + 1):
 		pygame.draw.line(screen, BLUE, (c * TILE_SIZE, 0), (c * TILE_SIZE, SCREEN_HEIGHT), 3)
 	#horizontal lines
-	for r in range(ROWS + 1):
+	for r in range(MAIN_ROWS + 1):
 		pygame.draw.line(screen, BLUE, (0, r * TILE_SIZE), (SCREEN_WIDTH, r * TILE_SIZE), 3)
 
+
+def draw_smaller_grid():
+	#vertical lines
+	for c in range(SMALL_COLS + 1):
+		pygame.draw.line(screen, LIGHTBLUE, (c * TILE_SIZE / 4, 0), (c * TILE_SIZE / 4, SCREEN_HEIGHT), 3)
+	#horizontal lines
+	for r in range(SMALL_ROWS + 1):
+		pygame.draw.line(screen, LIGHTBLUE, (0, r * TILE_SIZE / 4), (SCREEN_WIDTH, r * TILE_SIZE / 4), 3)
 
 
 run = True
 while run:
 	
 	draw_bkg()
-	draw_grid()
+	draw_smaller_grid()
+	draw_main_grid()
 
 	for event in pygame.event.get(): 
 		if event.type == pygame.QUIT:
