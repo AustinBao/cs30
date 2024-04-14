223e1811938451f288504a69326fb4b02422a430
Austin Bao <austin_dun@outlook.com>
2024-04-13 22:42:03 -0600

    game stages are working + tilemap works

--- averagehorsechicken/main.py
+++ averagehorsechicken/main.py
@@ -2,6 +2,8 @@ import pygame
 import os
 import player
 import button
+import time
+
 
 pygame.init()
 
@@ -26,14 +28,16 @@ CARDBOARD = (159, 135, 103)
 
 #define game variables
 isPregameOpen = True
-isPlaceItemOpen = True
+isItemChosen = True
 current_tile = 0
 number_of_players = 1
-TILE_SIZE = 100 
-MAIN_ROWS = SCREEN_HEIGHT // TILE_SIZE
-MAIN_COLS = SCREEN_WIDTH // TILE_SIZE
-SMALL_ROWS = SCREEN_HEIGHT // 4
-SMALL_COLS = SCREEN_WIDTH // 4
+selected_items = []
+TILE_SIZE_MAIN = 100
+TILE_SIZE_SMALL = TILE_SIZE_MAIN // 4
+MAIN_ROWS = SCREEN_HEIGHT // TILE_SIZE_MAIN
+MAIN_COLS = SCREEN_WIDTH // TILE_SIZE_MAIN
+SMALL_ROWS = SCREEN_HEIGHT // TILE_SIZE_SMALL
+SMALL_COLS = SCREEN_WIDTH // TILE_SIZE_SMALL
 PREGAME_WIDTH = 700
 PREGAME_HEIGHT = 600
 START_PREGAME_X = (SCREEN_WIDTH - PREGAME_WIDTH) // 2
@@ -41,7 +45,12 @@ START_PREGAME_Y = (SCREEN_HEIGHT - PREGAME_HEIGHT) // 2
 
 
 # Load player
-racoon = player.Player(150, 600, 0.08, "rac.png")
+player1_keys = {'left': pygame.K_a, 'right': pygame.K_d, 'up': pygame.K_w, 'down': pygame.K_s}
+player2_keys = {'left': pygame.K_LEFT, 'right': pygame.K_RIGHT, 'up': pygame.K_UP, 'down': pygame.K_DOWN}
+
+racoon = player.Player(150, 600, 0.08, "rac") # player 1
+iguana = player.Player(140, 600, 0.08, "ig") # player 2
+
 
 # Loading images
 background = pygame.image.load("imgs/background.png")
@@ -54,89 +63,128 @@ def draw_bkg():
 def draw_main_grid():
 	#vertical lines
 	for c in range(MAIN_COLS + 1):
-		pygame.draw.line(screen, BLUE, (c * TILE_SIZE, 0), (c * TILE_SIZE, SCREEN_HEIGHT), 3)
+		pygame.draw.line(screen, BLUE, (c * TILE_SIZE_MAIN, 0), (c * TILE_SIZE_MAIN, SCREEN_HEIGHT), 3)
 	#horizontal lines
 	for r in range(MAIN_ROWS + 1):
-		pygame.draw.line(screen, BLUE, (0, r * TILE_SIZE), (SCREEN_WIDTH, r * TILE_SIZE), 3)
+		pygame.draw.line(screen, BLUE, (0, r * TILE_SIZE_MAIN), (SCREEN_WIDTH, r * TILE_SIZE_MAIN), 3)
 
 
 def draw_smaller_grid():
 	#vertical lines
 	for c in range(SMALL_COLS + 1):
-		pygame.draw.line(screen, LIGHTBLUE, (c * TILE_SIZE / 4, 0), (c * TILE_SIZE / 4, SCREEN_HEIGHT), 3)
+		pygame.draw.line(screen, LIGHTBLUE, (c * TILE_SIZE_SMALL, 0), (c * TILE_SIZE_SMALL, SCREEN_HEIGHT), 3)
 	#horizontal lines
 	for r in range(SMALL_ROWS + 1):
-		pygame.draw.line(screen, LIGHTBLUE, (0, r * TILE_SIZE / 4), (SCREEN_WIDTH, r * TILE_SIZE / 4), 3)
+		pygame.draw.line(screen, LIGHTBLUE, (0, r * TILE_SIZE_SMALL), (SCREEN_WIDTH, r *TILE_SIZE_SMALL), 3)
 
 
 def draw_player(self, screen):
     screen.blit(self.image, (self.x, self.y))
 
 
-# load all item images for the pregame (not scaled)
 # place items are all scaled to match the grid
 place_item_list = []
 number_of_items = len(os.listdir('imgs/items')) - 1
 for i in range(number_of_items):
 	img = pygame.image.load(f'imgs/items/{i}.png')
-	scaled_img = pygame.transform.scale_by(img, 0.7)
+	if i == 5:
+		scaled_img = pygame.transform.scale(img, (TILE_SIZE_MAIN//2, TILE_SIZE_MAIN//2))
+	else:
+		scaled_img = pygame.transform.scale_by(img, 0.7)
 	place_item_list.append(scaled_img)
 
 # convert imgs into button objects
 button_list = []
 button_col = 0
 for i in range(len(place_item_list)):
-	tile_button = button.Button(START_PREGAME_X + (button_col * 130) + 50, START_PREGAME_Y + 150 , place_item_list[i], 1, i)
+	tile_button = button.Button(START_PREGAME_X + (button_col * 120) + 20 , START_PREGAME_Y + 150 , place_item_list[i], 1, i)
 	button_list.append(tile_button)
 	button_col += 1
 		
+# Empty tile list
+world_data = []
+for row in range(SMALL_ROWS):
+	r = [-1] * SMALL_COLS
+	world_data.append(r)
 
 
 def draw_pre_game():
 	pygame.draw.rect(screen, CARDBOARD, pygame.Rect(START_PREGAME_X, START_PREGAME_Y, PREGAME_WIDTH, PREGAME_HEIGHT))
 	
+def draw_world():
+	for y, row in enumerate(world_data):
+		for x, tile in enumerate(row):
+			if tile >= 0:
+				img = pygame.image.load(f'imgs/items/{tile}.png')
+				img = pygame.transform.scale(img, (TILE_SIZE_SMALL + 4,TILE_SIZE_SMALL + 4))
+				screen.blit(img, (x * TILE_SIZE_SMALL, y * TILE_SIZE_SMALL))
+
+
 
 run = True
 while run:
+	pos = pygame.mouse.get_pos()
+	x = pos[0] // TILE_SIZE_SMALL
+	y = pos[1] // TILE_SIZE_SMALL
 
 	if isPregameOpen:
-		draw_bkg()
+		draw_bkg() 
 		draw_pre_game()
 		selected_items = []
 		for current_count, b in enumerate(button_list):
 			if b.draw(screen):
 				selected_items.append(b.id)
-				print(selected_items)
 				if len(selected_items) == number_of_players:
 					isPregameOpen = False
 	else:
-		if isPlaceItemOpen:
+		if isItemChosen:
 			draw_bkg()
 			draw_smaller_grid()
 			draw_main_grid()
-			pos = pygame.mouse.get_pos()
+			
+			# keys = pygame.key.get_pressed()
+			# if keys[pygame.K_r]:
+			# 	img = pygame.transform.rotate(img, 90) 
+
 			for items in selected_items:
 				img = pygame.image.load(f'imgs/items/{items}.png')
+				
 				# If the items are long vertical items, scale differently
 				if items == 1 or items == 3: 
-					img = pygame.transform.scale(img, (25, TILE_SIZE))
+					img = pygame.transform.scale(img, (TILE_SIZE_SMALL, TILE_SIZE_MAIN))
 				# If items is crossbow
 				elif items == 0:
-					img = pygame.transform.scale(img, (25, TILE_SIZE//2))
+					img = pygame.transform.scale(img, (TILE_SIZE_SMALL, TILE_SIZE_MAIN//2))
 				# If items are one tile 
 				else:
-					img = pygame.transform.scale(img, (25, 25))
+					img = pygame.transform.scale(img, (TILE_SIZE_SMALL, TILE_SIZE_SMALL))
 				img.set_alpha(128)
-				screen.blit(img, (pos[0] - img.get_width(), pos[1] - img.get_height()))
-			
+				screen.blit(img, (pos[0] - img.get_width() + 10, pos[1] - img.get_height() + 10))
+				isItemChosen = False
+		
+			if 0 <= x < SMALL_COLS and 0 <= y < SMALL_ROWS:
+				if pygame.mouse.get_pressed()[0] == 1:
+					print(selected_items)
+					world_data[y][x] = selected_items[0]
+					print("Updated world_data at ({}, {}):".format(y, x))
+					selected_items.pop(0)
+					if len(selected_items) == 0:
+						isItemChosen = False
 		else:
+			draw_bkg()
+			draw_world()
+
 			draw_player(racoon, screen)
-			racoon.move()	
-			
+			draw_player(iguana, screen)
+			racoon.move(player1_keys)
+			iguana.move(player2_keys)
+	
+	
 	for event in pygame.event.get(): 
 		if event.type == pygame.QUIT:
 			run = False
 
+	draw_world()
 	
 	pygame.display.update()
 
--- averagehorsechicken/player.py
+++ averagehorsechicken/player.py
@@ -1,63 +1,57 @@
 import pygame
 
-
 class Player(pygame.sprite.Sprite):
+    Y_GRAVITY = 0.14
+    JUMP_HEIGHT = 7
+    Y_VELOCITY = JUMP_HEIGHT
 
-	# static variables to control jump
-	jumping = False
-	Y_GRAVITY = 0.6
-	JUMP_HEIGHT = 13
-	Y_VELOCITY = JUMP_HEIGHT
+    def __init__(self, x, y, scale, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.original_img = pygame.image.load(f'imgs/characters/{img}.png')
+        self.jumping_img = pygame.image.load(f'imgs/characters/{img}_jump.png')
+        self.scale = scale
+        self.x = x
+        self.y = y
+        self.jumping = False
+        self.image = self.transform_image(self.original_img)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
 
+    def transform_image(self, img, flip=False):
+        img = pygame.transform.flip(img, flip, False)
+        return pygame.transform.scale(img, (int(img.get_width() * self.scale), int(img.get_height() * self.scale)))
 
-	def __init__(self, x, y, scale, img):
-		pygame.sprite.Sprite.__init__(self)
-		self.original_img = pygame.image.load(f'imgs/characters/{img}')
-		self.jumping_img = pygame.image.load(f'imgs/characters/jumping.png')
-		self.scale = scale
-		self.x = x
-		self.y = y
-		self.image = pygame.transform.scale(self.original_img, (int(self.original_img.get_width() * scale), int(self.original_img.get_height() * scale)))
-		self.rect = self.image.get_rect()
-		self.rect.center = (x, y)
+    def update_image(self, keys, move_keys):
+        if self.jumping:
+            img = self.jumping_img
+            # flip becomes true when turning left thus flipping the image
+            flip = keys[move_keys['left']]
+        else:
+            img = self.original_img
+            flip = keys[move_keys['left']] and not keys[move_keys['right']]
+        self.image = self.transform_image(img, flip)
 
+    def update_position(self, keys, move_keys):
+        if keys[move_keys['left']]:
+            self.x -= 3
+        if keys[move_keys['right']]:
+            self.x += 3
+        if keys[move_keys['up']]:
+            self.jumping = True
+        if keys[move_keys['down']]:
+            self.y += 3
 
-	def move(self):
-	
-		keys = pygame.key.get_pressed()
-		if keys[pygame.K_SPACE] and keys[pygame.K_d]:
-			Player.jumping = True
-			self.x += 5
-		elif keys[pygame.K_SPACE] and keys[pygame.K_a]:
-			Player.jumping = True
-			self.x -= 5
-		elif keys[pygame.K_a]:
-			self.image = pygame.transform.flip(self.original_img, True, False)
-			self.image = pygame.transform.scale(self.image, (int(self.original_img.get_width() * self.scale), int(self.original_img.get_height() * self.scale)))
-			self.x -= 5
-		elif keys[pygame.K_d]:
-			self.image = pygame.transform.flip(self.original_img, False, False)
-			self.image = pygame.transform.scale(self.image, (int(self.original_img.get_width() * self.scale), int(self.original_img.get_height() * self.scale)))
-			self.x += 5
-		elif keys[pygame.K_w]:
-			self.y -= 5
-		elif keys[pygame.K_s]:
-			self.y += 5
-		elif keys[pygame.K_SPACE]:
-			Player.jumping = True
+    def handle_jumping(self):
+        if self.jumping:
+            self.y -= Player.Y_VELOCITY
+            Player.Y_VELOCITY -= Player.Y_GRAVITY
+            if Player.Y_VELOCITY < -Player.JUMP_HEIGHT:
+                self.jumping = False
+                Player.Y_VELOCITY = Player.JUMP_HEIGHT
 
-		
-		if Player.jumping:
-			if keys[pygame.K_a]:
-				self.image = pygame.transform.flip(self.jumping_img, True, False)
-				self.image = pygame.transform.scale(self.image, (int(self.jumping_img.get_width() * self.scale), int(self.jumping_img.get_height() * self.scale)))
-			else:
-				self.image = pygame.transform.scale(self.jumping_img, (int(self.jumping_img.get_width() * self.scale), int(self.jumping_img.get_height() * self.scale)))
-			self.y -= Player.Y_VELOCITY
-			Player.Y_VELOCITY -= Player.Y_GRAVITY
-			
-			if Player.Y_VELOCITY < -Player.JUMP_HEIGHT:
-				Player.jumping = False
-				self.image = pygame.transform.scale(self.original_img, (int(self.original_img.get_width() * self.scale), int(self.original_img.get_height() * self.scale)))
-				Player.Y_VELOCITY = Player.JUMP_HEIGHT
-
+    def move(self, move_keys):
+        keys = pygame.key.get_pressed()
+        self.update_position(keys, move_keys)
+        self.update_image(keys, move_keys)
+        self.handle_jumping()
+        self.rect.center = (self.x, self.y)
