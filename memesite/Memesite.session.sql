ae8d65ee8ab1f7fffbbc1219949dbac375e025b5
Austin Bao <austin_dun@outlook.com>
2024-05-13 22:36:24 -0600

    node.js funny buisness

---
+++ memesite/Memesite.session.sql
@@ -0,0 +1,7 @@
+--@block
+CREATE TABLE Users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
